ca3cdde271404f9a32a2449e061d60cef60b5e5c
chriszhang94 <chris@quantumsds.com>
2020-12-11 09:37:08 -0500

    arguments added

--- user_service/server.py
+++ user_service/server.py
@@ -1,17 +1,47 @@
 import grpc
 import logging
+import signal
+import sys
+import os
+import argparse
 from concurrent import futures
 from loguru import logger
-from user_service.proto import user_pb2_grpc, user_pb2
+
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, BASE_DIR)
+
+from user_service.proto import user_pb2_grpc
 from user_service.handler.user import UserServicer
 
 
+def onExit(signo, frame):
+    logger.info("Process Terminate")
+    sys.exit(0)
+
+
 def serve():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip',
+                        nargs="?",
+                        type=str,
+                        default="127.0.0.1",
+                        help="ip")
+    parser.add_argument('--port',
+                        nargs="?",
+                        type=int,
+                        default="50052",
+                        help="port")
+    args = parser.parse_args()
+
     logger.add("logs/user_service_{time}.log")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_pb2_grpc.add_UserServicer_to_server(UserServicer(), server)
-    server.add_insecure_port('[::]:50052')
-    logger.info("start service")
+
+    # 主进程退出信号监听
+    signal.signal(signal.SIGINT, onExit)
+    signal.signal(signal.SIGTERM, onExit)
+    server.add_insecure_port(f'[::]:{args.port}')
+    logger.info("start service {}:{}".format(args.ip, args.port))
     server.start()
     server.wait_for_termination()
 
